aa76292936f15fdb0cdcce3b9fc2ad2904d427ac
Rafael <rafaelchiti@gmail.com>
2022-07-27 18:45:33 +0200

    Different layout approach

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,25 +11,27 @@ export const HomePage = () => {
           return (
             <div
               key={index}
-              className="py-4 px-7 pb-16 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5mb-9"
+              // className="py-4 px-7 pb-16 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5mb-9"
+              className="p-2 h-[500px] w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5mb-9"
             >
               <NextLink href={`/photos/${photo.id}`}>
                 <a>
                   <div
                     key={index}
-                    style={{ aspectRatio: `${photo.aspectRatio}` }}
-                    className="relative bg-gray-6"
+                    // style={{ aspectRatio: `${photo.aspectRatio}` }}
+                    className="relative bg-gray-6 h-full w-full"
                   >
                     <NextImage
                       src={photo.url}
                       layout="fill"
+                      objectFit="cover"
                       alt={`Photo from ${photo.author}`}
                     />
                   </div>
                 </a>
               </NextLink>
 
-              <div className="pt-2">
+              {/* <div className="pt-2">
                 <NextLink
                   href={`/by/${photo.author.replace(" ", "-").toLowerCase()}`}
                 >
@@ -41,7 +43,7 @@ export const HomePage = () => {
                 <span className="text-gray-10 text-sm">
                   {format(photo.date, "MMM dd, yyyy")}
                 </span>
-              </div>
+              </div> */}
             </div>
           );
         })}
