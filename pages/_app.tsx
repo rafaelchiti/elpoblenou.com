5bbd74cc58aed583ea8d81cb80690a0851d58ab4
Rafael <rafaelchiti@gmail.com>
2022-07-29 08:50:07 +0200

    Add analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
-import "styles/globals.css";
+import * as React from "react";
 import type { AppProps } from "next/app";
 import { useScrollRestoration } from "src/utils/useScrollRestoration";
+import splitbee from "@splitbee/web";
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useScrollRestoration();
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === "production") splitbee.init();
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
