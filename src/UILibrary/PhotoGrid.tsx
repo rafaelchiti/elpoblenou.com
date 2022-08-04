2af56f7e23b99114184e45044653f3789e460535
Rafael <rafaelchiti@gmail.com>
2022-08-04 13:07:54 +0200

    Layout (#1)
    * Home page layout
    
    * Tweaks on the layout

---
+++ src/UILibrary/PhotoGrid.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { PhotoType } from "src/constants/photos";
+import { format } from "date-fns";
+import { CalendarIcon } from "@radix-ui/react-icons";
+
+export const PhotoGrid = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) => {
+  return <div className="sm:flex sm:flex-wrap">{children}</div>;
+};
+
+export const PhotoGridItem = ({ photo }: { photo: PhotoType }) => {
+  return (
+    <div
+      className="
+                w-full h-[75vh]  px-2 pb-16 mb-4
+                sm:h-[40vh] sm:w-1/2  sm:px-3
+                md:w-1/2 md:h-[45vh] md:px-4 
+                lg:w-1/3 lg:px-6 
+                xl:w-1/4 
+                2xl:w-1/5
+                "
+    >
+      <NextLink href={`/photos/${photo.id}`}>
+        <a>
+          <div
+            // style={{ aspectRatio: `${photo.aspectRatio}` }}
+            className="relative 
+                    w-full h-full"
+          >
+            <NextImage
+              src={photo.url}
+              layout="fill"
+              objectFit="cover"
+              alt={`Photo from ${photo.author}`}
+            />
+          </div>
+        </a>
+      </NextLink>
+
+      <div className="pt-1">
+        <NextLink href={`/by/${photo.author.replace(" ", "-").toLowerCase()}`}>
+          <a className="text-gray-10 text-md hover:underline hover:cursor-pointer">
+            {photo.author}
+          </a>
+        </NextLink>
+        <div className="flex items-center gap-2">
+          <span className="text-gray-10  text-sm">
+            <CalendarIcon className="" />
+          </span>
+          <span className="text-gray-10 text-xs">
+            {format(photo.date, "MMM dd, yyyy")}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -4,6 +4,7 @@ import NextImage from "next/image";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { PHOTOS, PhotoType } from "src/constants/photos";
+import { PhotoGrid, PhotoGridItem } from "src/UILibrary/PhotoGrid";
 
 export const CollectionPage = ({
   photos,
@@ -36,42 +37,11 @@ export const CollectionPage = ({
 
       <div className="mt-10" />
 
-      <div className="flex flex-wrap items-end">
+      <PhotoGrid>
         {photos.map((photo, index) => {
-          return (
-            <div
-              key={index}
-              className="py-4 px-7 pb-16 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5mb-9"
-            >
-              <NextLink href={`/photos/${photo.id}`}>
-                <a>
-                  <div
-                    key={index}
-                    style={{ aspectRatio: `${photo.aspectRatio}` }}
-                    className="relative bg-gray-6"
-                  >
-                    <NextImage
-                      src={photo.url}
-                      layout="fill"
-                      alt={`Photo from ${photo.author}`}
-                    />
-                  </div>
-                </a>
-              </NextLink>
-
-              <div className="pt-2">
-                <span className="text-gray-10 text-sm hover:underline hover:cursor-pointer">
-                  {photo.author}
-                </span>
-                <span className="text-gray-10 text-sm px-3">{"·"}</span>
-                <span className="text-gray-10 text-sm">
-                  {format(photo.date, "MMM dd, yyyy")}
-                </span>
-              </div>
-            </div>
-          );
+          return <PhotoGridItem key={index} photo={photo} />;
         })}
-      </div>
+      </PhotoGrid>
     </div>
   );
 };
